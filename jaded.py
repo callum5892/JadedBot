fc2038cf427447263fa41ba34b99e1ffebb72686
Virtual- <luke@tonnochi.net>
2020-08-17 21:27:17 +0000

    Working configparser

--- jaded.py
+++ jaded.py
@@ -102,8 +102,8 @@ async def on_message(message):
 config = configparser.ConfigParser()
 config.read('configfile')
 
-TOKEN = config['JadedBot']['TOKEN']
-REDDIT_ID = config['JadedBot']['REDDIT_ID']
-REDDIT_SECRET = config['JadedBot']['REDDIT_SECRET']
+TOKEN = config['JadedBot']['TOKEN'].replace('"', '')
+REDDIT_ID = config['JadedBot']['REDDIT_ID'].replace('"', '')
+REDDIT_SECRET = config['JadedBot']['REDDIT_SECRET'].replace('"', '')
 
 bot.run(TOKEN)
